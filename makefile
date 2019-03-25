7c30061bd2cfa6a36912402f954e90590b0dba5d
Azrrael <vcamargo.e@gmail.com>
2019-03-24 19:42:57 -0500

    Pokemon Picker + Json lib added + Pokemon data added + user interface added

--- makefile
+++ makefile
@@ -1,7 +1,8 @@
 COMPILER = g++
+INCLUDE_PATH = ./include
+LIBS_PATH = ./libs
 
 main: src/main.cpp
-	$(COMPILER) $? -o bin/$@.o
-
+	$(COMPILER) $? -I $(INCLUDE_PATH) -L $(LIBS_PATH) -o bin/$@.o
 clean:
 	rm bin/*.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <fstream>
+#include <string>
+#include <streambuf>
+#include <iostream>
+
+#include <nlohmann/json.hpp>
+
+using namespace std;
+using json = nlohmann::json;
 
 int main(){
-    printf("Hello World!\n");
+    std::ifstream pokemon_file("./data/pokemons.json");
+    json pokemon_json;
+    pokemon_file >> pokemon_json;
+    cout << "Pick a pokemon number (Max 151)\n";
+    uint8_t number;
+    cin >> number;
+    cout << "----------\n";
+    json pokemon = pokemon_json[std::to_string(number)];
+    cout << pokemon.dump(4) << endl;
     return 0;
 }
